390a0fdf4e663c24ef891a2a21409aa009280313
Allison Griffin <gallie8699@gmail.com>
2026-04-20 15:01:40 -0400

    first commit

---
+++ 4-6/animals.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+echo "Your username is " . $_POST["form-username"] .
+", and you like " . $_POST["form-favAnimal"] . ". However, you find a " . $_POST["form-scaryAnimal"] . "scary. "
+
+
+
+
+?>
+
+
+
+
+
+
+
+</body>
+</html>
